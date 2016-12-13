1581f2529b793f68874e066fdb06fb8222e17638
gxf <381332153@qq.com>
2016-12-13 16:21:21 +0800

    init

--- js/controllers/announceCtrl.js
+++
@@ -1,6 +0,0 @@
-angular.module('starter.controllers').controller('AnnounceCtrl',
-  function($rootScope,$scope,$state,$location,index,API,$log) {
-    $scope.$on('$ionicView.enter', function (e) {
-      $log.debug(API.getUserInfo());
-    });
-  });
